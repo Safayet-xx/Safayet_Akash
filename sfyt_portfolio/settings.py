e46659b9c72805ee46b25f99b019eae135f7526f
Safayet <safayetakash@gmail.com>
2025-07-22 19:04:05 +0600

    prog

--- sfyt_portfolio/settings.py
+++ sfyt_portfolio/settings.py
@@ -188,11 +188,18 @@ WSGI_APPLICATION = 'sfyt_portfolio.wsgi.application'
 
 import dj_database_url
 
+
+
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
     )
 }
+
+# Force SSL connection if using Postgres
+if DATABASES['default']['ENGINE'] == 'django.db.backends.postgresql':
+    DATABASES['default']['OPTIONS'] = {'sslmode': 'require'}
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
