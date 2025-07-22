1e0049143c05fa3d9589757114e53bf18f66b0da
Safayet <safayetakash@gmail.com>
2025-07-22 23:27:56 +0600

    Updated settings for deployment

--- sfyt_portfolio/settings.py
+++ sfyt_portfolio/settings.py
@@ -69,7 +69,15 @@ import dj_database_url
 
 DATABASES = {
     
-        'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+        'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+    
+
+        #'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+
+
    # 'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
     #'default': {
         #'ENGINE': 'django.db.backends.postgresql',
